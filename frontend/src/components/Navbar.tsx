998f0aa5bd0da956dc4cf1e3017ddc7ce344fd2d
RayanR000 <rane.ra@northeastern.edu>
2026-03-05 00:04:25 -0500

    add courses tab at top right

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -25,6 +25,21 @@ const Navbar = () => {
           </svg>
           Professors
         </a>
+        <a href="/courses" className="nav-link">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z" />
+            <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" />
+          </svg>
+          Courses
+        </a>
         <button className="signin-btn">Sign In</button>
       </div>
     </nav>
